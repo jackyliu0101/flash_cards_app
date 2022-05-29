a394aede57d7b62957fbc4e77dbaa657dc7e36c5
Jacky Liu <jackyliu@Jackys-MacBook-Pro.local>
2022-05-29 01:00:17 -0700

    added heading and searchbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import SearchBox from './components/SearchBox';
+import FlashCardsHeading from './components/FlashCardsHeading'
 
 function App() {
   return (
     <div>
-      Hello world!
+      <div>
+        <FlashCardsHeading heading = "Flash Cards" />
+      </div>
+      <div>
+        <SearchBox />
+      </div>
+      <button> Add Flash Card </button>
     </div>
   );
 }
